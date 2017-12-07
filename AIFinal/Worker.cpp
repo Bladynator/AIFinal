a97d6e2ad94d2cb7330c34025733d4aadb8aca78
frederick <f.ph.vandermeulen@gmail.com>
2017-12-07 20:38:36 +0000

    MAJOR : added movement code for the worker

--- AIFinal/Worker.cpp
+++ AIFinal/Worker.cpp
@@ -3,3 +3,60 @@
 Worker::~Worker()
 {
 }
+
+
+void Worker::Update() {
+	Wander();
+	
+	//If collision with the player
+	//Then destroy instance of itself
+
+
+}
+
+/// <summary>
+/// The main method for walking around
+/// </summary>
+void Worker::Wander() {
+
+	if (!HasReachedDestination())
+	{
+		//Move the Worker towards the target
+		if (xPos >= randomXPos)
+			xPos += xVel;
+		else
+			xPos -= xVel;
+		if (yPos >= randomYPos)
+			yPos += yVel;
+		else
+			yPos -= yVel;
+		//Rotate the worker
+		orientation += rotation;
+		//Calculate the position based on the rotation
+
+		//Set the new Position and Rotation
+		xPos += float(yVel * cos((double)orientation * 0.0174532925));
+		yPos += float(yVel * sin((double)orientation * 0.0174532925));
+
+		//Finally, sets the new position and rotation in the shape
+		shape.setPosition(sf::Vector2f(xPos, yPos));
+		shape.setRotation(orientation);
+
+	}
+	else {
+		//Setting the new coordinates
+		randomXPos = rand() % 100;
+		randomYPos = rand() % 100;
+
+	}
+}
+
+/// <summary>
+/// Determines whether the worker has reached the destination.
+/// </summary>
+/// <returns>
+///   <c>true</c> if the worker has reached destination;
+/// </returns>
+bool Worker::HasReachedDestination() {
+	return (xPos == randomXPos) && (yPos == randomYPos);
+}
--- AIFinal/Worker.h
+++ AIFinal/Worker.h
@@ -4,14 +4,32 @@
 
 class Worker : public NPC
 {
+private:
+	float xPos = 0;
+	float yPos = 0;
+	float xVel = 0.03f;
+	float yVel = 0.03f;
+
+	float randomXPos = rand() % 500;
+	float randomYPos = rand() % 500;
+
+	sf::Vector2f newPosition;
+	float orientation = 0;
+	float rotation = 0.005f;
+
+
+	bool HasReachedDestination();
+
 public:
 	Worker(sf::String textureName, sf::Vector2f size, sf::Vector2f spawnLocation) : NPC(textureName, size, spawnLocation)
 	{
-
+		newPosition = sf::Vector2f(randomXPos, randomYPos);
 	}
+
 	~Worker();
 
-	float velocity;
+	
+	void Update();
 	void Wander();
 };
 
